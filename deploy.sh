938c21fcd801e569e17df5ca014bf1557e6173ce
Rafael Baptista <rafabap100@gmail.com>
2022-11-09 00:56:23 -0300

    update on shell script deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-!/bin/bash
+#!/bin/bash
 
-tar -a -cf AmplifyDeploymentLambda.zip $(ls) # not working when resource handler tries to unzip in cloudformation
+tar --exclude "\.git*" -cvf AmplifyDeploymentLambda.zip . # recommended to use zip, doesn't work in windows
 aws s3api put-object --bucket demo-bucket-rafa --key AmplifyDeploymentLambda.zip --body AmplifyDeploymentLambda.zip
 aws cloudformation deploy --template-file lambda.yml --stack-name AmplifyLambdaFunction
 rm AmplifyDeploymentLambda.zip
